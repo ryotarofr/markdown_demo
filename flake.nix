6af36c4b882675f3317c2aa6b91e1dad613689ab
Ryotaro Fujii <ryoryo.fr0608@gmail.com>
2025-03-01 14:48:12 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,22 +7,33 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";  # 必要に応じてシステムを変更
+      system = "x86_64-linux";  # 利用するシステムに合わせる
       pkgs = import nixpkgs { inherit system; };
     in {
-      packages.default = pkgs.stdenv.mkDerivation {
+      # defaultPackage として定義すれば、nix run で実行可能になります
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
         pname = "remark-markdown-unist";
         version = "0.1";
         src = self;
         phases = [ "installPhase" ];
         installPhase = ''
-          mkdir -p $out
-          cp -r * $out/
+          mkdir -p $out/bin
+          # ここでシンプルなシェルスクリプトを作成（例: 実行時にメッセージを表示）
+          echo '#!/bin/sh' > $out/bin/run
+          echo 'echo "remark-markdown-unist flake running!"' >> $out/bin/run
+          chmod +x $out/bin/run
         '';
       };
 
+      # オプション: アプリケーションとしても提供する場合
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${self.outputs.packages.x86_64-linux.default}/bin/run";
+      };
+
+      # 開発用シェルを提供する場合
       devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [ /* 必要なパッケージ */ ];
+        buildInputs = [ ];
       };
     };
 }
