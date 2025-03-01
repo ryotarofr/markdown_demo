2c4b21d23f8bd0559383453c7a7958eafd02cb44
Ryotaro Fujii <ryoryo.fr0608@gmail.com>
2025-03-01 14:55:22 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    snacks-nvim = {
+      url = "github:folke/snacks.nvim";
+      flake = false;
+    };
+    trouble-nvim = {
+      url = "github:folke/trouble.nvim";
+      flake = false;
+    };
+    typr-nvim = {
+      url = "github:nvzone/typr";
+      flake = false;
+    };
+    volt-nvim = {
+      url = "github:nvzone/volt";
+      flake = false;
+    };
+    showkeys-nvim = {
+      url = "github:nvzone/showkeys";
+      flake = false;
+    };
+    avante-nvim = {
+      url = "github:yetone/avante.nvim";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, ... }:
